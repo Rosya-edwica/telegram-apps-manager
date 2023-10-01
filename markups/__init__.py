9fdcec9a88a56e2ad065a1fc5d98bedaceed3d4c
Rosya-edwica <yunoshev.dev@gmail.com>
2023-10-01 16:24:22 +0300

    добавил обработчики поступи онлайн

--- markups/__init__.py
+++
@@ -1,7 +0,0 @@
-from markups.gpt_positions import *
-from markups.gpt_skills import *
-from markups.openedu import *
-from markups.postupi import *
-from markups.vacancies import *
-from markups.menu import *
-from markups.gpt import *
